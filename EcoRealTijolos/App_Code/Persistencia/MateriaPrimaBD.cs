04ed74715e55b6c33a39dad6c8b7cb591dceb277
dgochavess <dgochavespellegrine@gmail.com>
2018-10-05 19:19:06 -0300

    Criação Classes MateriaPrima e MateriaPrimaBD. Adicionados pacotes bootstrapp, jquery e css

---
+++ EcoRealTijolos/App_Code/Persistencia/MateriaPrimaBD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcoRealTijolos.App_Code.Persistencia
+{
+    public class MateriaPrimaBD
+    {
+    }
+}
