c53336055775e785773e7e8840aceb11f17e57f0
DesireeFraga95 <fragadeh.a@gmail.com>
2018-10-06 11:25:10 -0300

    Inicio da Etapa de Pedidos

---
+++ EcoRealTijolos/App_Code/Classes/Pedido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcoRealTijolos.App_Code.Classes
+{
+    public class Pedido
+    {
+        public int Codigo { get; set; }
+        public DateTime Data { get; set; }
+        public String EnderecoEntrega { get; set; }
+        public String Observacao { get; set; }
+
+        //Relacionamentos 
+
+        //Public Cliente cliente { get; set; } (1 pedido só pode ter 1 um cliente, e um cliente pode ter N vários pedidos)
+        // public List <Produto> Produtos { get; set; } (1 pedido pode ter N varios produtos, e um produtos pode estar em N varios pedidos)
+
+        //construtor
+        public Pedido()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
---
+++ EcoRealTijolos/App_Code/Persistencia/PedidoBD.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EcoRealTijolos.App_Code.Classes;
+using System.Data;
+using FATEC;
+
+namespace EcoRealTijolos.App_Code.Persistencia
+{
+    /// <summary>
+    /// Summary description for PedidoBD
+    /// </summary>
+    public class PedidoBD
+    {
+        //métodos
+        //insert
+        public bool Insert(Pedido pedido)
+        {
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            string sql = "INSERT INTO tbl_pedido (ped_data, ped_endereco, ped_obs) VALUES (?data, ?enderecoEntrega, ?observacao)";
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command(sql, objConexao);
+            objCommand.Parameters.Add(Mapped.Parameter("?data", pedido.Data));
+            objCommand.Parameters.Add(Mapped.Parameter("?endereco", pedido.EnderecoEntrega));
+            objCommand.Parameters.Add(Mapped.Parameter("?observacao", pedido.Observacao));
+            objCommand.ExecuteNonQuery();
+            objConexao.Close();
+            objCommand.Dispose();
+            objConexao.Dispose();
+            return true;
+        }
+
+        //selectall
+        public DataSet SelectAll()
+        {
+            DataSet ds = new DataSet();
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            System.Data.IDataAdapter objDataAdapter;
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command("SELECT * FROM tbl_pedido", objConexao);
+            objDataAdapter = Mapped.Adapter(objCommand);
+            objDataAdapter.Fill(ds);
+            objConexao.Close();
+            objCommand.Dispose();
+            objConexao.Dispose();
+            return ds;
+        }
+        //select
+        public Pedido Select(int id)
+        {
+            Pedido obj = null;
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            System.Data.IDataReader objDataReader;
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command("SELECT * FROM tbl_pedido WHERE ped_id = ?codigo", objConexao);
+            objCommand.Parameters.Add(Mapped.Parameter("?codigo", id));
+            objDataReader = objCommand.ExecuteReader();
+            while (objDataReader.Read())
+            {
+                obj = new Pedido();
+                obj.Codigo = Convert.ToInt32(objDataReader["ped_codigo"]);
+                obj.Data = Convert.ToDateTime(objDataReader["ped_data"]);
+                obj.EnderecoEntrega = Convert.ToString(objDataReader["ped_endereco"]);
+                obj.Observacao = Convert.ToString(objDataReader["ped_obs"]);
+            }
+            objDataReader.Close();
+            objConexao.Close();
+            objCommand.Dispose();
+            objConexao.Dispose();
+            objDataReader.Dispose();
+            return obj;
+        }
+
+        //update
+        public bool Update(Pedido pedido)
+        {
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            string sql = "UPDATE tbl_pedido SET ped_data=?data, ped_endereco=?enderecoEntrega, ped_obs=?observacao WHERE ped_id=?codigo";
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command(sql, objConexao);
+            objCommand.Parameters.Add(Mapped.Parameter("?data", pedido.Data));
+            objCommand.Parameters.Add(Mapped.Parameter("?enderecoEntrega", pedido.EnderecoEntrega));
+            objCommand.Parameters.Add(Mapped.Parameter("?observacao", pedido.Observacao));
+            objCommand.Parameters.Add(Mapped.Parameter("?codigo", pedido.Codigo));
+            objCommand.ExecuteNonQuery();
+            objConexao.Close();
+            objCommand.Dispose();
+            objConexao.Dispose();
+            return true;
+        }
+        //delete
+        public bool Delete(int id)
+        {
+            System.Data.IDbConnection objConexao;
+            System.Data.IDbCommand objCommand;
+            string sql = "DELETE FROM tbl_pedido WHERE ped_id=?codigo";
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command(sql, objConexao);
+            objCommand.Parameters.Add(Mapped.Parameter("?codigo", id));
+
+            objCommand.ExecuteNonQuery();
+            objConexao.Close();
+            objCommand.Dispose();
+            objConexao.Dispose();
+            return true;
+        }
+        //construtor
+        public PedidoBD()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
